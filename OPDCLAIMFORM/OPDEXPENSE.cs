34ddda5a575821affb673f56499bab909ab39630
Abdus Samad <samad.qureshi@qordata.com>
2020-03-16 09:48:34 +0500

    Changes related to Field Addition

--- OPDCLAIMFORM/OPDEXPENSE.cs
+++ OPDCLAIMFORM/OPDEXPENSE.cs
@@ -25,14 +25,12 @@ namespace OPDCLAIMFORM
     
         public int OPDEXPENSE_ID { get; set; }
         public string EMPLOYEE_EMAILADDRESS { get; set; }
-
         [DisplayName("Employee Name")]
         [Required(ErrorMessage = "The Employee Name is required.")]
         [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Incorrect Employee Name")] 
         public string EMPLOYEE_NAME { get; set; }
         public string EMPLOYEE_DEPARTMENT { get; set; }
         public string CLAIM_MONTH { get; set; }
-
         [DisplayName("Total Amount")]
         [Required(ErrorMessage = "The Total Amount is required.")]
         [Range(0, Int32.MaxValue, ErrorMessage = "Invalid Number")]
@@ -49,27 +47,15 @@ namespace OPDCLAIMFORM
         public Nullable<bool> MANAGEMENT_APPROVAL { get; set; }
         public Nullable<System.DateTime> MANAGEMENT_APPROVAL_DATE { get; set; }
         public string MANAGEMENT_NAME { get; set; }
-
-      
         public Nullable<System.DateTime> DATE_ILLNESS_NOTICED { get; set; }
-
         public Nullable<System.DateTime> DATE_RECOVERY { get; set; }
-
         public string DIAGNOSIS { get; set; }
         public Nullable<bool> CLAIMANT_SUFFERED_ILLNESS { get; set; }
         public Nullable<System.DateTime> CLAIMANT_SUFFERED_ILLNESS_DATE { get; set; }
         public string CLAIMANT_SUFFERED_ILLNESS_DETAILS { get; set; }
-
-        
         public string HOSPITAL_NAME { get; set; }
-
-        
         public string DOCTOR_NAME { get; set; }
-
-
         public Nullable<System.DateTime> PERIOD_CONFINEMENT_DATE_FROM { get; set; }
-
-   
         public Nullable<System.DateTime> PERIOD_CONFINEMENT_DATE_TO { get; set; }
         public Nullable<bool> DRUGS_PRESCRIBED_BOOL { get; set; }
         public string DRUGS_PRESCRIBED_DESCRIPTION { get; set; }
@@ -83,6 +69,8 @@ namespace OPDCLAIMFORM
         public string MANAGEMENT_EMAILADDRESS { get; set; }
         public string STATUS { get; set; }
         public string CLAIM_YEAR { get; set; }
+        public Nullable<decimal> TOTAL_AMOUNT_APPROVED { get; set; }
+        public string EXPENSE_NUMBER { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<OPDEXPENSE_IMAGE> OPDEXPENSE_IMAGE { get; set; }
