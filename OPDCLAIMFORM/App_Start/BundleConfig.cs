3cf0f326e9417237f034aa861aec1637009e48e7
Arsalan Qamar <arsalan.qamar@qordata.com>
2020-03-18 15:16:12 +0500

    add css/js reference

--- OPDCLAIMFORM/App_Start/BundleConfig.cs
+++ OPDCLAIMFORM/App_Start/BundleConfig.cs
@@ -8,31 +8,42 @@ namespace OPDCLAIMFORM
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            // Core Required
+            bundles.Add(new ScriptBundle("~/bundles/core").Include(
+                "~/Scripts/core/popper.min.js",
+                "~/Scripts/core/bootstrap-material-design.min.js",
+                "~/Scripts/plugins/perfect-scrollbar.jquery.min.js"));
 
-            // Boorstrap file upload. 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap-file").Include(
-                                  "~/Scripts/script-bootstrap-file.js"));
+            // Plugins for the Projects
+            bundles.Add(new ScriptBundle("~/Scripts/plugins").Include(
+                "~/Scripts/plugins/moment.min.js",
+                "~/Scripts/plugins/sweetalert2.js",
+                //"~/Scripts/plugins/jquery.validate.min.js",
+                "~/Scripts/plugins/bootstrap-selectpicker.js",
+                "~/Scripts/plugins/bootstrap-datetimepicker.min.js",
+                "~/Scripts/plugins/jquery.dataTables.min.js",
+                "~/Scripts/plugins/jquery.dataTables.responsive.js",
+                "~/Scripts/plugins/jasny-bootstrap.min.js",
+                "~/Scripts/core.min.js",
+                "~/Scripts/plugins/arrive.min.js",
+                "~/Scripts/plugins/bootstrap-notify.js",
+                "~/Scripts/material-dashboard.js"));
 
+            // Project style files
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Content/material-dashboard.min.css",
+                "~/Content/site.css"));
 
             // Bootstrap file upload.
-            bundles.Add(new StyleBundle("~/Content/Bootstrap-file/css").Include(
-                                 "~/Content/style/style-bootstrap-file.css"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap-file").Include("~/Scripts/script-bootstrap-file.js"));
+            bundles.Add(new StyleBundle("~/Content/Bootstrap-file/css").Include("~/Content/style/style-bootstrap-file.css"));
         }
     }
 }
