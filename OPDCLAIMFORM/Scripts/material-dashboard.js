f6dd78b3e97aafe49fe687ca29490e53d5caa104
Arsalan Qamar <arsalan.qamar@qordata.com>
2020-03-18 14:24:06 +0500

    add new javascript

---
+++ OPDCLAIMFORM/Scripts/material-dashboard.js
@@ -0,0 +1,537 @@
+/*!
+
+ =========================================================
+ * Material Dashboard - v2.1.2
+ =========================================================
+
+ * Product Page: https://www.creative-tim.com/product/material-dashboard
+ * Copyright 2020 Creative Tim (http://www.creative-tim.com)
+
+ * Designed by www.invisionapp.com Coded by www.creative-tim.com
+
+ =========================================================
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ */
+
+(function() {
+  isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
+
+  if (isWindows) {
+    // if we are on windows OS we activate the perfectScrollbar function
+    $('.sidebar .sidebar-wrapper, .main-panel, .main').perfectScrollbar();
+
+    $('html').addClass('perfect-scrollbar-on');
+  } else {
+    $('html').addClass('perfect-scrollbar-off');
+  }
+})();
+
+
+var breakCards = true;
+
+var searchVisible = 0;
+var transparent = true;
+
+var transparentDemo = true;
+var fixedTop = false;
+
+var mobile_menu_visible = 0,
+  mobile_menu_initialized = false,
+  toggle_initialized = false,
+  bootstrap_nav_initialized = false;
+
+var seq = 0,
+  delays = 80,
+  durations = 500;
+var seq2 = 0,
+  delays2 = 80,
+  durations2 = 500;
+
+$(document).ready(function() {
+
+  $('body').bootstrapMaterialDesign();
+
+  $sidebar = $('.sidebar');
+
+  md.initSidebarsCheck();
+
+  window_width = $(window).width();
+
+  // check if there is an image set for the sidebar's background
+  md.checkSidebarImage();
+  
+  md.initMinimizeSidebar();
+
+  //    Activate bootstrap-select
+  if ($(".selectpicker").length != 0) {
+    $(".selectpicker").selectpicker();
+  }
+
+  //  Activate the tooltips
+  $('[rel="tooltip"]').tooltip();
+
+  $('.form-control').on("focus", function() {
+    $(this).parent('.input-group').addClass("input-group-focus");
+  }).on("blur", function() {
+    $(this).parent(".input-group").removeClass("input-group-focus");
+  });
+
+  // remove class has-error for checkbox validation
+  $('input[type="checkbox"][required="true"], input[type="radio"][required="true"]').on('click', function() {
+    if ($(this).hasClass('error')) {
+      $(this).closest('div').removeClass('has-error');
+    }
+  });
+  
+  md.initFormExtendedDatetimepickers();
+  
+  md.initDataTable();
+  
+  md.initShowSwal();
+
+});
+
+$(document).on('click', '.navbar-toggler', function() {
+  $toggle = $(this);
+
+  if (mobile_menu_visible == 1) {
+    $('html').removeClass('nav-open');
+
+    $('.close-layer').remove();
+    setTimeout(function() {
+      $toggle.removeClass('toggled');
+    }, 400);
+
+    mobile_menu_visible = 0;
+  } else {
+    setTimeout(function() {
+      $toggle.addClass('toggled');
+    }, 430);
+
+    var $layer = $('<div class="close-layer"></div>');
+
+    if ($('body').find('.main-panel').length != 0) {
+      $layer.appendTo(".main-panel");
+
+    } else if (($('body').hasClass('off-canvas-sidebar'))) {
+      $layer.appendTo(".wrapper-full-page");
+    }
+
+    setTimeout(function() {
+      $layer.addClass('visible');
+    }, 100);
+
+    $layer.click(function() {
+      $('html').removeClass('nav-open');
+      mobile_menu_visible = 0;
+
+      $layer.removeClass('visible');
+
+      setTimeout(function() {
+        $layer.remove();
+        $toggle.removeClass('toggled');
+
+      }, 400);
+    });
+
+    $('html').addClass('nav-open');
+    mobile_menu_visible = 1;
+
+  }
+
+});
+
+// activate collapse right menu when the windows is resized
+$(window).resize(function() {
+  md.initSidebarsCheck();
+
+  // reset the seq for charts drawing animations
+  seq = seq2 = 0;
+  
+});
+
+md = {
+  misc: {
+    navbar_menu_visible: 0,
+    active_collapse: true,
+    disabled_collapse_init: 0,
+  },
+
+  checkSidebarImage: function() {
+    $sidebar = $('.sidebar');
+    image_src = $sidebar.data('image');
+
+    if (image_src !== undefined) {
+      sidebar_container = '<div class="sidebar-background" style="background-image: url(' + image_src + ') "/>';
+      $sidebar.append(sidebar_container);
+    }
+  },
+
+  showNotification: function(from, align) {
+    type = ['', 'info', 'danger', 'success', 'warning', 'rose', 'primary'];
+
+    color = Math.floor((Math.random() * 6) + 1);
+
+    $.notify({
+      icon: "add_alert",
+      message: "Welcome to <b>Material Dashboard Pro</b> - a beautiful admin panel for every web developer."
+
+    }, {
+      type: type[color],
+      timer: 3000,
+      placement: {
+        from: from,
+        align: align
+      }
+    });
+  },
+
+  initFormExtendedDatetimepickers: function() {
+    $('.datetimepicker').datetimepicker({
+      icons: {
+        time: "fa fa-clock-o",
+        date: "fa fa-calendar",
+        up: "fa fa-chevron-up",
+        down: "fa fa-chevron-down",
+        previous: 'fa fa-chevron-left',
+        next: 'fa fa-chevron-right',
+        today: 'fa fa-screenshot',
+        clear: 'fa fa-trash',
+        close: 'fa fa-remove'
+      }
+    });
+
+    $('.datepicker').datetimepicker({
+      date: new Date(),
+      format: 'MM/DD/YYYY',
+      icons: {
+        time: "fa fa-clock-o",
+        date: "fa fa-calendar",
+        up: "fa fa-chevron-up",
+        down: "fa fa-chevron-down",
+        previous: 'fa fa-chevron-left',
+        next: 'fa fa-chevron-right',
+        today: 'fa fa-screenshot',
+        clear: 'fa fa-trash',
+        close: 'fa fa-remove'
+      }
+    });
+
+    $('.timepicker').datetimepicker({
+      //          format: 'H:mm',    // use this format if you want the 24hours timepicker
+      format: 'h:mm A', //use this format if you want the 12hours timpiecker with AM/PM toggle
+      icons: {
+        time: "fa fa-clock-o",
+        date: "fa fa-calendar",
+        up: "fa fa-chevron-up",
+        down: "fa fa-chevron-down",
+        previous: 'fa fa-chevron-left',
+        next: 'fa fa-chevron-right',
+        today: 'fa fa-screenshot',
+        clear: 'fa fa-trash',
+        close: 'fa fa-remove'
+
+      }
+    });
+  },
+
+  initSidebarsCheck: function() {
+    if ($(window).width() <= 991) {
+      if ($sidebar.length != 0) {
+        md.initRightMenu();
+      }
+    }
+  },
+
+  checkFullPageBackgroundImage: function() {
+    $page = $('.full-page');
+    image_src = $page.data('image');
+
+    if (image_src !== undefined) {
+      image_container = '<div class="full-page-background" style="background-image: url(' + image_src + ') "/>'
+      $page.append(image_container);
+    }
+  },
+  
+  initMinimizeSidebar: function() {
+
+    $('#minimizeSidebar').click(function() {
+      var $btn = $(this);
+
+      if (md.misc.sidebar_mini_active == true) {
+        $('body').removeClass('sidebar-mini');
+        md.misc.sidebar_mini_active = false;
+      } else {
+        $('body').addClass('sidebar-mini');
+        md.misc.sidebar_mini_active = true;
+      }      
+    });
+  },
+
+  checkScrollForTransparentNavbar: debounce(function() {
+    if ($(document).scrollTop() > 260) {
+      if (transparent) {
+        transparent = false;
+        $('.navbar-color-on-scroll').removeClass('navbar-transparent');
+      }
+    } else {
+      if (!transparent) {
+        transparent = true;
+        $('.navbar-color-on-scroll').addClass('navbar-transparent');
+      }
+    }
+  }, 17),
+
+
+  initRightMenu: debounce(function() {
+    $sidebar_wrapper = $('.sidebar-wrapper');
+
+    if (!mobile_menu_initialized) {
+      $navbar = $('nav').find('.navbar-collapse').children('.navbar-nav');
+
+      mobile_menu_content = '';
+
+      nav_content = $navbar.html();
+
+      nav_content = '<ul class="nav navbar-nav nav-mobile-menu">' + nav_content + '</ul>';
+
+      navbar_form = $('nav').find('.navbar-form').get(0).outerHTML;
+
+      $sidebar_nav = $sidebar_wrapper.find(' > .nav');
+
+      // insert the navbar form before the sidebar list
+      $nav_content = $(nav_content);
+      $navbar_form = $(navbar_form);
+      $nav_content.insertBefore($sidebar_nav);
+      $navbar_form.insertBefore($nav_content);
+
+      $(".sidebar-wrapper .dropdown .dropdown-menu > li > a").click(function(event) {
+        event.stopPropagation();
+
+      });
+
+      // simulate resize so all the charts/maps will be redrawn
+      window.dispatchEvent(new Event('resize'));
+
+      mobile_menu_initialized = true;
+    } else {
+      if ($(window).width() > 991) {
+        // reset all the additions that we made for the sidebar wrapper only if the screen is bigger than 991px
+        $sidebar_wrapper.find('.navbar-form').remove();
+        $sidebar_wrapper.find('.nav-mobile-menu').remove();
+
+        mobile_menu_initialized = false;
+      }
+    }
+  }, 200),
+  
+  initDataTable: function() {
+
+    $('#datatables').DataTable({
+        "pagingType": "full_numbers",
+        "lengthMenu": [
+          [10, 25, 50, -1],
+          [10, 25, 50, "All"]
+        ],
+        responsive: true,
+        language: {
+          search: "_INPUT_",
+          searchPlaceholder: "Search records",
+        },
+		"initComplete": function(settings, json) {
+			$(".custom-select").selectpicker();
+		}
+	});	
+
+	var table = $('#datatable').DataTable();
+  },
+  
+  initShowSwal: function(type, id) {
+        if (type == 'basic') {
+            swal({
+                title: "Here's a message!",
+                buttonsStyling: false,
+                confirmButtonClass: "btn btn-success"
+            }).catch(swal.noop)
+
+        } else if (type == 'title-and-text') {
+            swal({
+                title: "Here's a message!",
+                text: "It's pretty, isn't it?",
+                buttonsStyling: false,
+                confirmButtonClass: "btn btn-info"
+            }).catch(swal.noop)
+
+        } else if (type == 'success-message') {
+            swal({
+                title: "Good job!",
+                text: "You clicked the button!",
+                buttonsStyling: false,
+                confirmButtonClass: "btn btn-success",
+                type: "success"
+            }).catch(swal.noop)
+
+        } else if (type == 'warning-message-and-confirmation') {
+            swal({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonClass: 'btn btn-success',
+                cancelButtonClass: 'btn btn-danger',
+                confirmButtonText: 'Yes, delete it!',
+                buttonsStyling: false
+            }).then(function() {
+                swal({
+                    title: 'Deleted!',
+                    text: 'Your file has been deleted.',
+                    type: 'success',
+                    confirmButtonClass: "btn btn-success",
+                    buttonsStyling: false
+                })
+            }).catch(swal.noop)
+        } else if (type == 'warning-message-and-cancel') {
+            swal({
+                title: 'Are you sure?',
+                text: 'You will not be able to recover this imaginary file!',
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Yes, delete it!',
+                cancelButtonText: 'No, keep it',
+                confirmButtonClass: "btn btn-success",
+                cancelButtonClass: "btn btn-danger",
+                buttonsStyling: false
+            }).then(function(result) {
+				if (result.value) {
+					swal({
+						title: 'Deleted!',
+						text: 'Your imaginary file has been deleted.',
+						type: 'success',
+						confirmButtonClass: "btn btn-success",
+						buttonsStyling: false
+					}).catch(swal.noop)
+				} else if (
+					/* Read more about handling dismissals below */
+					result.dismiss === Swal.DismissReason.cancel
+					) {
+					swal({
+                        title: 'Cancelled',
+                        text: 'Your imaginary file is safe :)',
+                        type: 'error',
+                        confirmButtonClass: "btn btn-info",
+                        buttonsStyling: false
+                    }).catch(swal.noop)
+				  }
+            })
+
+        } else if (type == 'custom-html') {
+            swal({
+                title: 'HTML example',
+                buttonsStyling: false,
+                confirmButtonClass: "btn btn-success",
+                html: 'You can use <b>bold text</b>, ' +
+                    '<a href="http://github.com">links</a> ' +
+                    'and other HTML tags'
+            }).catch(swal.noop)
+
+        } else if (type == 'auto-close') {
+            swal({
+                title: "Auto close alert!",
+                text: "I will close in 2 seconds.",
+                timer: 2000,
+                showConfirmButton: false
+            }).catch(swal.noop)
+        } else if (type == 'input-field') {
+            swal({
+                title: 'Input something',
+                html: '<div class="form-group">' +
+                    '<input id="input-field" type="text" class="form-control" />' +
+                    '</div>',
+                showCancelButton: true,
+                confirmButtonClass: 'btn btn-success',
+                cancelButtonClass: 'btn btn-danger',
+                buttonsStyling: false
+            }).then(function(result) {
+                swal({
+                    type: 'success',
+                    html: 'You entered: <strong>' +
+                        $('#input-field').val() +
+                        '</strong>',
+                    confirmButtonClass: 'btn btn-success',
+                    buttonsStyling: false
+
+                })
+            }).catch(swal.noop)
+        }
+    }
+}
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this,
+      args = arguments;
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    }, wait);
+    if (immediate && !timeout) func.apply(context, args);
+  };
+};
+
+
+/* Create Form */
+$(document).ready(function () {
+	$("#CLAIMANT_SUFFERED_ILLNESS_DATE").attr("disabled", "disabled");
+	$("#CLAIMANT_SUFFERED_ILLNESS_DETAILS").attr("disabled", "disabled");
+	$("#DRUGS_PRESCRIBED_DESCRIPTION").attr("disabled", "disabled");
+	
+	$("#CLAIMANT_SUFFERED_ILLNESS").on("changed.bs.select", function(e, clickedIndex, newValue, oldValue) {
+		if ($("#CLAIMANT_SUFFERED_ILLNESS option:selected").text() == "True") {
+			$("#CLAIMANT_SUFFERED_ILLNESS_DATE").removeAttr("disabled");
+			$("#CLAIMANT_SUFFERED_ILLNESS_DETAILS").removeAttr("disabled");
+		}
+		else {
+			$("#CLAIMANT_SUFFERED_ILLNESS_DATE").attr("disabled", "disabled");
+			$("#CLAIMANT_SUFFERED_ILLNESS_DETAILS").attr("disabled", "disabled");
+		}
+	});
+  
+	$("#DRUGS_PRESCRIBED_BOOL").on("changed.bs.select", function(e, clickedIndex, newValue, oldValue) {
+		if ($("#DRUGS_PRESCRIBED_BOOL option:selected").text() == "True") {
+			$("#DRUGS_PRESCRIBED_DESCRIPTION").removeAttr("disabled");
+		}
+		else {
+			$("#DRUGS_PRESCRIBED_DESCRIPTION").attr("disabled", "disabled");
+		}
+	});
+
+	$('.form-control').on("click blur", function() {
+		if ($(this).hasClass('input-validation-error')) {
+			$(this).parents('.form-group').addClass('has-danger');
+
+		} else {
+			$(this).parents('.form-group').removeClass('has-danger');
+		}
+	});
+	
+	$('.create-btn').on("click", function() {
+		$('.form-control').each(function(){
+			if ($(this).hasClass('input-validation-error')) {
+				$(this).parents('.form-group').addClass('has-danger');
+
+			} else {
+				$(this).parents('.form-group').removeClass('has-danger');
+			}
+		});		
+	});
+	
+});
